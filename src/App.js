560b38e76bb4da28a659cd2fefc6283eec2ecf07
LiekeM <liekevandermade@gmail.com>
2023-06-18 16:34:15 +0200

    FEAT | Added search engine in Dictionary.js, updated main and footer in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from "./dictionary.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Dictionary</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          SheCodes student project coded by Lieke van der Made
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching... for the definition of "${keyword}"`);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
