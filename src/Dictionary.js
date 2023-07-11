47c4f55c18402ffc04678e4516b71df1e0fedfd3
LiekeM <liekevandermade@gmail.com>
2023-07-11 12:08:12 +0200

    FIX | error handling 404, added alert

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,42 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
+// import { createClient } from "pexels";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("icecream");
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [errorMessage, setErrorMessage] = useState(null);
 
   function handleResponse(response) {
-    setResults(response.data[0]);
+    if (response.status === 200) {
+      setErrorMessage(null);
+      setResults(response.data[0]);
+    } else {
+      alert("word");
+    }
+  }
+  // function handlePexelResponse(response) {
+  //   console.log(response);
+  // }
+
+  function handleError(error) {
+    console.log(error.response.data.message);
+    setErrorMessage(error.response.data.message);
+    alert(error.response.data.message);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleResponse).catch(handleError);
+
+    // const PexelApi = createPexelApi(
+    //   "GN1zCwCkPKzSWgNxBfR9NL04o5oleuTomzdeHR1RqMGVCMpxpTWri8O6"
+    // );
+    //   let pexelApiUrl = `https://api.pexels.com/v1/search?query=${keyword}$per_page=1`;
+    //   axios.get(pexelApiUrl).then(handlePexelResponse);
   }
 
   function onSubmit(event) {
@@ -43,11 +66,16 @@ export default function Dictionary() {
             />
           </form>
         </section>
-        <Results results={results} />
+        {results && (
+          <>
+            <Results results={results} />
+            <Photos results={results} />
+          </>
+        )}
       </div>
     );
   } else {
     load();
-    return "Loading..";
+    return "loading";
   }
 }
